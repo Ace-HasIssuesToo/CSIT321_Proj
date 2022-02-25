28ca555d1685675884bcc229d40741e9d113cc6c
brandon9696 <brandicity@yahoo.com.sg>
2022-02-26 00:43:47 +0800

    Updated last facing direction
    After moving, player will now face last known direction

--- CSIT321/Assets/playerMove.cs
+++ CSIT321/Assets/playerMove.cs
@@ -16,13 +16,20 @@ public class playerMove : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
+        //if a movement key is being pressed ...
+        if (movement != Vector2.zero)
+        {
+            //update horizontal/vertical accordingly
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }   //else Horizontal and Vertical will not update each frame, leaving it at last recorded value 
+
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //movement.normalized so character does not move faster diagonally
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
     }
 }

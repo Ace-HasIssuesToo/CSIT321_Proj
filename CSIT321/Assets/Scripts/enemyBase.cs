20bc45652ec33c9b0c6ddea6fafa3bfd4686e87b
Ace-HasIssuesToo <No.1Twisties@gmail.com>
2022-03-16 03:10:04 +0800

    Starting on EnemyBase

--- CSIT321/Assets/Scripts/enemyBase.cs
+++ CSIT321/Assets/Scripts/enemyBase.cs
@@ -6,19 +6,38 @@ public class enemyBase : MonoBehaviour
 {
     float moveSpeed = 5f;
     public Rigidbody2D rb;
+   
+    private enum state
+    {
+        Normal,
+        Caution,
+        Alert,
+    }
+
+    private state currentState;
 
     Vector2 movement;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = state.Normal;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch(currentState)
+        {
+            default:
+            case state.Normal:
+                break;
 
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+            case state.Caution:
+                break;
+
+            case state.Alert:
+                break;
+        }
     }
 }
